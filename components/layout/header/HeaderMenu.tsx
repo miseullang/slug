8d180ec7dbc3afc0e6c8cc3d038e1e0137b38eb1
miseullang <miseullang@gmail.com>
2025-11-15 23:26:01 +0900

    🔨 Refactor: 메뉴 배열 추가

--- components/layout/header/HeaderMenu.tsx
+++ components/layout/header/HeaderMenu.tsx
@@ -1,18 +1,36 @@
 import { HomeIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
+const menuItems = [
+  {
+    href: "/",
+    icon: HomeIcon,
+    label: "Home",
+  },
+  {
+    href: "/",
+    icon: HomeIcon,
+    label: "Home",
+  },
+  {
+    href: "/",
+    icon: HomeIcon,
+    label: "Home",
+  },
+];
+
 export default function HeaderMenu() {
   return (
     <nav>
       <ul className="flex items-center gap-4">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <li key={index}>
+        {menuItems.map((item) => (
+          <li key={item.href}>
             <Link
-              href="/"
+              href={item.href}
               className="header-link inline-flex items-center gap-2 pb-1"
             >
-              <HomeIcon className="w-6 h-6" />
-              <span>Home</span>
+              <item.icon className="w-6 h-6" />
+              <span>{item.label}</span>
             </Link>
           </li>
         ))}
